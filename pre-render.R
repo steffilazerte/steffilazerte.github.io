4439718a028458291abf35327d93eb828d3bd7ce
Steffi LaZerte <steffi@steffi.ca>
2024-04-18 15:01:44 -0500

    Update wording and projects, add email button

--- pre-render.R
+++ pre-render.R
@@ -17,13 +17,27 @@ inst <- tribble(~short, ~long,
                 "UofM", "University of Manitoba",
                 "TRU", "Thompson Rivers University")
 
-jobs <- read_ods("~/Documents/Resumes/jobs.ods") %>%
+packages <- read_ods("~/Documents/Resumes/jobs.ods", sheet = "Packages") |>
+  left_join(read_ods("~/Documents/Resumes/jobs.ods", sheet = "Contacts"), by = "contact") |>
   mutate(across(everything(), ~str_replace_all(., setNames(inst$long, inst$short))))
+workflows <- read_ods("~/Documents/Resumes/jobs.ods", sheet = "Workflows") |>
+  bind_rows(read_ods("~/Documents/Resumes/jobs.ods", sheet = "Analyses")) |>
+  left_join(read_ods("~/Documents/Resumes/jobs.ods", sheet = "Contacts"), by = "contact") |>
+  mutate(across(everything(), ~str_replace_all(., setNames(inst$long, inst$short))))
+
+workshops <- read_ods("~/Documents/Resumes/jobs.ods", sheet = "Workshops") |>
+  mutate(across(everything(), ~str_replace_all(., setNames(inst$long, inst$short))))
+extra <- read_ods("~/Documents/Resumes/jobs.ods", sheet = "Extra") |>
+  bind_rows(read_ods("~/Documents/Resumes/jobs.ods", sheet = "Teaching and Community")) |>
+  mutate(across(everything(), ~str_replace_all(., setNames(inst$long, inst$short))))
+
+
 
 fmt_gt <- function(d) {
   gt(d) %>%
     fmt_markdown(columns = any_of(c("content", "title"))) %>%
     cols_align(align = "left") %>%
     tab_options(column_labels.hidden = TRUE,
-                table.width = "100%")
+                table.width = "100%") |>
+    cols_width(1 ~ "20%")
 }
